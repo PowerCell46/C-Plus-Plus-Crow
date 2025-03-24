ae75d89d5d395aacb5e6cd9461fdf79db2d6078b
Peter Gerdzhikov <makotsevo.fan@gmail.com>
2025-03-24 21:08:54 +0200

    Adding endpoint for fetching all of the questions

--- main.cpp
+++ main.cpp
@@ -13,20 +13,9 @@ public:
 int main() {
     crow::SimpleApp app;
 
-
-    // TODO: GET REQUEST
+    // GET REQUEST
     // -----------------------------------------------------------------------------------------------------------------
-    CROW_ROUTE(app, "/")
-            .name("hello")([] {
-                return "Hello World!";
-            });
 
-    CROW_ROUTE(app, "/about")
-    ([]() {
-        return "About Crow example.";
-    });
-
-    // simple json response
     CROW_ROUTE(app, "/json")
     ([] {
         crow::json::wvalue x({{"message", "Hello, World!"}});
@@ -34,23 +23,7 @@ int main() {
         return x;
     });
 
-    CROW_ROUTE(app, "/json-initializer-list-constructor")
-    ([] {
-        crow::json::wvalue r({
-            {"first", "Hello world!"}, /* stores a char const* hence a json::type::String */
-            {"second", std::string("How are you today?")}, /* stores a std::string hence a json::type::String. */
-            {"third", 54}, /* stores an int (as 54 is an int literal) hence a std::int64_t. */
-            {"fourth", (int64_t) 54l}, /* stores a long (as 54l is a long literal) hence a std::int64_t. */
-            {"fifth", 54u}, /* stores an unsigned int (as 54u is a unsigned int literal) hence a std::uint64_t. */
-            {"sixth", (uint64_t) 54ul},
-            /* stores an unsigned long (as 54ul is an unsigned long literal) hence a std::uint64_t. */
-            {"seventh", 2.f}, /* stores a float (as 2.f is a float literal) hence a double. */
-            {"eighth", 2.}, /* stores a double (as 2. is a double literal) hence a double. */
-            {"ninth", nullptr}, /* stores a std::nullptr hence json::type::Null . */
-            {"tenth", true} /* stores a bool hence json::type::True . */
-        });
-        return r;
-    });
+    CROW_ROUTE(app, "/questions/get-all")(SurveyController::getQuestions);
 
     // json list response
     CROW_ROUTE(app, "/json_list")
--- src/SurveyController.h
+++ src/SurveyController.h
@@ -1,12 +1,10 @@
-#ifndef SURVEYCONTROLLER_H
-#define SURVEYCONTROLLER_H
+#pragma once
 #include "../crow_all.h"
 
 
 class SurveyController {
 public:
     static crow::response submitQuestion(const crow::request &req);
+
+    static crow::json::wvalue getQuestions();
 };
-
-
-#endif
